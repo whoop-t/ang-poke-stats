0019e94415707cd5c757f87296ea6ceb0bc7b656
whoop-t <tommymallis@gmail.com>
2020-04-25 12:44:13 -0700

    Added routes, added more style and grid for responsiveness

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { PokelistMainComponent } from './pokelist-main/pokelist-main/pokelist-main.component';
+import { PokemonComponent } from './pokelist-main/pokemon/pokemon.component';
+
+// Routes for app
+const routes: Routes = [
+  { path: '', redirectTo: '/pokelist', pathMatch: 'full' },
+  { path: 'pokelist', component: PokelistMainComponent },
+  { path: 'pokemon/:name', component: PokemonComponent },
+];
 
 @NgModule({
   declarations: [AppComponent, PokelistMainComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent],
 })
